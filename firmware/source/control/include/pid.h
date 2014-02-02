899e3414eab218401df3f7cec77224585a509c6b
Kyle McGahee <kmcgahee@ksu.edu>
2014-02-01 21:14:05 -0600

    Fixed bug in PID where time wasn't being taken into account.  Also taking credit for files because (1) I refactored them quite a bit and mainly (2) If any one has questions or if anything is wrong I'll take the blame.

--- firmware/source/control/include/pid.h
+++ firmware/source/control/include/pid.h
@@ -1,87 +1,85 @@
-/**********************************************************************
+/****************************************************************************************
 * File: pid.h
 *
 * Description: Header file for pid.cpp
 *
-* Created: 11/11/2013, by Brian Blankenau and Alex Marsh
-**********************************************************************/
+* Created: 02/01/2013, by Kyle McGahee
+****************************************************************************************/
 
 #ifndef PID_INCLUDED_H
 #define PID_INCLUDED_H
 
-/*---------------------------------------------------------------------
-*                             INCLUDES
-*--------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------
+*                                       INCLUDES
+*--------------------------------------------------------------------------------------*/
 
-/*---------------------------------------------------------------------
-*                             CONSTANTS
-*--------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------
+*                                      CONSTANTS
+*--------------------------------------------------------------------------------------*/
 
-/*---------------------------------------------------------------------
-*                               TYPES
-*--------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------
+*                                        TYPES
+*--------------------------------------------------------------------------------------*/
 
-/*----------------------------------------------------------------------
-*                              CLASSES
-*--------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------
+*                                       CLASSES
+*--------------------------------------------------------------------------------------*/
 
-/**********************************************************************
+/******************************************************************************
 * Class: PID
 *
 * Description: Generic Proportional/ Integral/ Derivative controller.
-**********************************************************************/
+******************************************************************************/
 class PID
 {
 public: // Methods
 
+    // Constructor
     PID
     (
-        float kp,              // Proportional Gain
-        float ki,              // Integral Gain
-        float kd,              // Derivative Gain
-        float sat_high,        // Upper Output Saturation Limit
-        float sat_low,         // Lower Output Saturation Limit
-        float int_sat_high,    // Upper Error Integral Saturation Limit
-        float int_sat_low      // Lower Error Integral Saturation Limit
-    ); // PID Constructor
+        float kp,                        // Proportional Gain
+        float ki,                        // Integral Gain
+        float kd,                        // Derivative Gain
+        float saturation_high,           // Upper Output Saturation Limit
+        float saturation_low,            // Lower Output Saturation Limit
+        float integral_saturation_high,  // Upper Error Integral Saturation Limit
+        float integral_saturation_low    // Lower Error Integral Saturation Limit
+    );
 
     // Returns the controller output for a given error and error derivative input. If
     // derivative error control is to be used, it is the responsibility of the user to
     // calculate the derivative value before calling this function.
     float Calculate
     (
-        float error,
-        float derivative
+        float  error,
+        float  derivative_error,
+        double delta_time
     );
 
-    // Same description as the Calculate function, but ScaleCalculate scales its output
+    // Same description as the Calculate function, but ScaledCalculate scales its output
     // between -1 and 1 based on the saturation limits.
-    float ScaleCalculate
+    float ScaledCalculate
     (
-        float error,
-        float derivative
+        float  error,
+        float  derivative_error,
+        double delta_time
     );
 
-    // Setter functions for private variables
+    // Setters
     void set_kp(float kp) {this->kp = kp;}
     void set_ki(float ki) {this->ki = ki;}
     void set_kd(float kd) {this->kd = kd;}
 
-    // Getter functions for private variables
-    float get_kp() const {return kp;}
-    float get_ki() const {return ki;}
-    float get_kd() const {return kd;}
-
 private: // Variables
 
-    float kp;           // Proportional Gain
-    float ki;           // Integral Gain
-    float kd;           // Derivative Gain
-    float sat_high;     // Saturation for high output limits
-    float sat_low;      // Saturation for low output limits
-    float int_sat_high; // Anti wind-up saturation
-    float int_sat_low;  // Anti wind-up saturation
-    float error_sum;    // Integral error
+    float kp;                       // Proportional Gain
+    float ki;                       // Integral Gain
+    float kd;                       // Derivative Gain
+    float saturation_high;          // Saturation for high output limits
+    float saturation_low;           // Saturation for low output limits
+    float integral_saturation_high; // Anti wind-up saturation
+    float integral_saturation_low;  // Anti wind-up saturation
+    float error_sum;                // Integral error
 
 }; //PID
 
--- firmware/source/control/pid.cpp
+++ firmware/source/control/pid.cpp
@@ -1,109 +1,117 @@
-/**********************************************************************
+/***************************************************************************************
 * File: pid.cpp
 *
-* Description: Function definitions for the PID class.
+* Description: Implementation of a generic Proportional, Integral, Derivative controller
+*              with saturation.
 *
-* Created: 11/11/2013, by Brian Blankenau and Alex Marsh
-**********************************************************************/
+* Created: 02/01/2014, by Kyle McGahee
+****************************************************************************************/
 
-/*---------------------------------------------------------------------
-*                             INCLUDES
-*--------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------
+*                                       INCLUDES
+*--------------------------------------------------------------------------------------*/
 
 #include "pid.h"
 #include "util_math.h"
 
-/*---------------------------------------------------------------------
-*                         LITERAL CONSTANTS
-*--------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------
+*                                   LITERAL CONSTANTS
+*--------------------------------------------------------------------------------------*/
 
-/*---------------------------------------------------------------------
-*                              TYPES
-*--------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------
+*                                        TYPES
+*--------------------------------------------------------------------------------------*/
 
-/*---------------------------------------------------------------------
-*                         MEMORY CONSTANTS
-*--------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------
+*                                   MEMORY CONSTANTS
+*--------------------------------------------------------------------------------------*/
 
-/*---------------------------------------------------------------------
-*                            VARIABLES
-*--------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------
+*                                      VARIABLES
+*--------------------------------------------------------------------------------------*/
 
-/*---------------------------------------------------------------------
-*                          CLASS METHODS
-*--------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------
+*                                     PROCEDURES
+*--------------------------------------------------------------------------------------*/
 
 /**********************************************************************
-* Function: PID - Constructor
+* Function: PID
 *
-* Description: Initializes fields for new instance of PID object.
+* Description: Constructor
 **********************************************************************/
 PID::PID
     (
-        float kp,              // Proportional Gain
-        float ki,              // Integral Gain
-        float kd,              // Derivative Gain
-        float sat_high,        // Upper Output Saturation Limit
-        float sat_low,         // Lower Output Saturation Limit
-        float int_sat_high,    // Upper Error Integral Saturation Limit
-        float int_sat_low      // Lower Error Integral Saturation Limit
+        float kp,                        // Proportional Gain
+        float ki,                        // Integral Gain
+        float kd,                        // Derivative Gain
+        float saturation_high,           // Upper Output Saturation Limit
+        float saturation_low,            // Lower Output Saturation Limit
+        float integral_saturation_high,  // Upper Error Integral Saturation Limit
+        float integral_saturation_low    // Lower Error Integral Saturation Limit
     ):
     kp(kp),
     ki(ki),
     kd(kd),
-    sat_high(sat_high),
-    sat_low(sat_low),
-    int_sat_high(int_sat_high),
-    int_sat_low(int_sat_low),
+    saturation_high(saturation_high),
+    saturation_low(saturation_low),
+    integral_saturation_high(integral_saturation_high),
+    integral_saturation_low(integral_saturation_low),
     error_sum(0)
 {
-}// PID() - Constructor
+}// PID::PID() - Constructor
 
 /*****************************************************************************
 * Function: Calculate
 *
-* Description: Performs PID calculation by using the Calculate() function.
-*              Scales output by using the saturation limits, and returns the
-*              result.
-* Note: this function does not update the scaled_output variable.
+* Description: Returns output of PID controller calculation using updated
+*              error, derivative error and change in time since last run.
 *****************************************************************************/
-float PID::Calculate(float error, float derivative_error)
+float PID::Calculate
+    (
+        float  error,
+        float  derivative_error,
+        double delta_time
+    )
 {
-    // Add on error to integral error summation.
-    error_sum += error;
+    error_sum += error * delta_time;
 
     // Ensure integral term is between saturation limits.
-    error_sum = CapBounds(error_sum, int_sat_low, int_sat_high);
+    error_sum = CapBounds(error_sum, integral_saturation_low, integral_saturation_high);
 
     // Calculate controller output from given inputs.
     float result = (kp * error) + (ki * error_sum) + (kd * derivative_error);
 
-    // Ensure controller output is between saturation limits
-    result = CapBounds(result, sat_low, sat_high);
+    // Ensure controller output is between saturation limits.
+    result = CapBounds(result, saturation_low, saturation_high);
 
    return result;
 
-} // Calculate()
+} // PID::Calculate()
 
 /*****************************************************************************
-* Function: ScaleCalculate
+* Function: ScaledCalculate
 *
 * Description: Performs PID calculation by using the error and derivative error
 *              inputs. Returns the output scaled from -1 to 1 based on the
 *              saturation limits. e.g. Sat limits +/-5, if Calculate() output
-*              is 3, ScaleCalculate() output is (3-(5+(-5)/2))/((5-(-5)/2)) = 0.6
+*              is 3, ScaledCalculate() output is (3-(5+(-5)/2))/((5-(-5)/2)) = 0.6
 *****************************************************************************/
-float PID::ScaleCalculate(float error, float derivative)
+float PID::ScaledCalculate
+    (
+        float  error,
+        float  derivative_error,
+        double delta_time
+    )
 {
-    float result = Calculate(error, derivative);
+    float result = Calculate(error, derivative_error, delta_time);
 
     // Scale output
-    float midpoint = (sat_high + sat_low) / 2.0f;
-    float range = (sat_high - sat_low) / 2.0f;
+    float midpoint = (saturation_high + saturation_low) / 2.0f;
+    float range    = (saturation_high - saturation_low) / 2.0f;
 
     // Scales between -1 and 1, where 0 is the midpoint of the saturation range
     float scaled_output = (result - midpoint) / range;
 
     return scaled_output;
 
-} // Calculate()
+} // PID::ScaledCalculate()
